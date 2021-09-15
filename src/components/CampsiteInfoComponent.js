32b562d523c02bd455e3a2259e2c4400f19778ad
Stwahirwa <stwahirwa91@gmail.com>
2021-09-14 19:41:05 -0500

    incoporating Redux Middleware and Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -17,6 +17,7 @@ import {
 import { Link } from "react-router-dom";
 import Modal from "reactstrap/lib/Modal";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 function RenderCampsite({ campsite }) {
   return (
@@ -58,6 +59,26 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+        <div className="container">
+            <div className="row">
+                <Loading />
+            </div>
+        </div>
+    );
+}
+if (props.errMess) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        </div>
+    );
+}
   if (props.campsite) {
     return (
       <div className="container">
